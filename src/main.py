03197573ee25afa1c27757799c04d8c755f1bcad
LisaCantrell <f.panda@gmail.com>
2025-01-30 12:02:04 -0600

    add textnode module. add main module

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+# import textnode
+from textnode import TextNode
+
+def main():
+    test_text_node = TextNode("sampletext", "normal", "https://www.dummysite.com")
+    print(test_text_node)
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,20 @@ class TextType(Enum):
     CODE = 'code'
     LINKS = 'links'
     IMAGES = 'images'
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(first_node, second_node):
+        is_equal = (
+            first_node.text == second_node.text 
+            and first_node.text_type == second_node.text_type
+            and first_node.url == second_node.url)
+        return is_equal
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        
